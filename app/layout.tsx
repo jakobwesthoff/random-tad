16dd62bfe181695ac8bbfbe760a66cb6cf81ad2b
Jakob Westhoff <jakob@westhoffswelt.de>
2025-03-10 21:27:34 +0100

    Add icons

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,6 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Random TAD - Trek am Dienstag Randomizer",
   description: "Discover random episodes from your favorite Star Trek podcast",
-  // manifest: "/manifest.json",
   appleWebApp: {
     capable: true,
     statusBarStyle: "black-translucent",
@@ -34,7 +33,6 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="apple-touch-icon" sizes="180x180" href="/icons/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
       </head>
--- app/manifest.ts
+++ app/manifest.ts
@@ -2,23 +2,25 @@ import type { MetadataRoute } from "next";
 
 export default function manifest(): MetadataRoute.Manifest {
   return {
-    name: "Random TAD",
+    name: "RandomTAD",
     short_name: "RandomTAD",
     description: "To boldy go where no Podcast has gone before.",
     start_url: "/",
     display: "standalone",
     background_color: "#000000",
-    theme_color: "#000000",
+    theme_color: "#fbc112",
     icons: [
       {
-        src: "/icon-192x192.png",
+        src: "/icons/web-app-manifest-192x192.png",
         sizes: "192x192",
         type: "image/png",
+        purpose: "maskable",
       },
       {
-        src: "/icon-512x512.png",
+        src: "/icons/web-app-manifest-512x512.png",
         sizes: "512x512",
         type: "image/png",
+        purpose: "maskable",
       },
     ],
   };
