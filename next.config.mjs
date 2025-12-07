5af8efa45d7e2fd560f0bc1f4c1c345292f45525
Jakob Westhoff <jakob@westhoffswelt.de>
2025-12-07 12:29:02 +0100

    Upgraded to nextjs 16

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
-  },
-  experimental: {
-    webpackBuildWorker: true,
-    parallelServerBuildTraces: true,
-    parallelServerCompiles: true,
   },
 };
 
